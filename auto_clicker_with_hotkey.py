8e7f68258a4ff111dd7e9a568b2cfbe67840920b
mncrftfrcnm <64576881+mncrftfrcnm@users.noreply.github.com>
2023-11-20 09:25:55 +0400

    Add files via upload

--- auto_clicker_with_hotkey.py
+++ auto_clicker_with_hotkey.py
@@ -1,11 +1,15 @@
-import keyboard, pyautogui
+import keyboard, pyautogui, time
 
 def is_hotkey_pressed(key):
     # Check if Ctrl, Alt, and H keys are all pressed
     return keyboard.is_pressed(key)
 def is_button_released(key):
     return not keyboard.is_pressed(key)
+pressing = -1
 while True:
     if is_hotkey_pressed('alt+c'):
-        pyautogui.click(button='middle')
+        pressing = pressing * -1
+        time.sleep(0.1)
+    if pressing == 1:
+        pyautogui.click(clicks=2)
     
